5a1960303c2131a45f64e2307be19dca5c3a132c
Vatsal Patel <vatsal4011@gmail.com>
2026-02-28 16:27:52 +0530

    tailwind typography plugin

--- algomind-frontend/tailwind.config.js
+++ algomind-frontend/tailwind.config.js
@@ -1,4 +1,7 @@
+import typography from '@tailwindcss/typography';
+
 export default {
     darkMode: ['class'],
     content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './features/**/*.{ts,tsx}'],
+    plugins: [typography],
 }
